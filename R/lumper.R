eeb6da5111fd9e18fdf69bd0023e0029569a1013
vandews9 <sebastiaan.vandewoude@basf.com>
2019-10-29 11:16:16 +0100

    add docker parse functionality through joyent/node-docker-file-parser

---
+++ R/lumper.R
@@ -0,0 +1,35 @@
+#' \code{lumper} package
+#'
+#' Read and write Dockerfiles in R
+#'
+#' See the README on
+#' \href{https://github.com/svdwoude/lumper}{GitHub}
+#'
+#' @docType package
+#' @name lumper
+#' @importFrom dplyr %>%
+#' @importFrom magrittr %$%
+#' @importFrom rlang %||%
+#' @importFrom vctrs %0%
+NULL
+
+
+## quiets concerns of R CMD check re: the .'s that appear in pipelines
+if (getRversion() >= "2.15.1")
+  utils::globalVariables(c(
+    "."
+  ))
+
+
+#' parse Dockerfile
+#' @param Dockerfile string; content Dockerfile
+#' @importFrom tibble as_tibble
+#' @importFrom V8 v8
+#' @export
+read_dockerfile <- function(Dockerfile) {
+  ct <- v8()
+  ct$source(system.file("js/parser.js", package = "lumper"))
+  ct$call("parse", Dockerfile)  %>% as_tibble()
+}
+
+
