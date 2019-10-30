006cd8d3e5d483bf9e2e44b68f918f67f1d777bd
vandews9 <sebastiaan.vandewoude@basf.com>
2019-10-30 14:35:04 +0100

    include comments

--- R/lumper.R
+++ R/lumper.R
@@ -24,12 +24,17 @@ if (getRversion() >= "2.15.1")
 #' parse Dockerfile
 #' @param Dockerfile string; content Dockerfile
 #' @importFrom tibble as_tibble
+#' @importFrom dplyr mutate
+#' @importFrom purrr map_chr
 #' @importFrom V8 v8
 #' @export
 read_dockerfile <- function(Dockerfile) {
   ct <- v8()
   ct$source(system.file("js/parser.js", package = "lumper"))
-  ct$call("parse", Dockerfile)  %>% as_tibble()
+  ct$call("parse", Dockerfile,  list(includeComments = TRUE)) %>%
+    as_tibble() %>%
+    mutate(
+      raw = ifelse(name == "COMMENT", args, raw),
+      raw = map_chr(raw, ~.x)
+    )
 }
-
-
